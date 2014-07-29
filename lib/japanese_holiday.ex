111980e2ffb7e2d71fba55c4369865e393c84789
Joe-noh <goflb.jh@gmail.com>
2014-07-29 22:32:06 +0900

    set japanese name

--- lib/japanese_holiday.ex
+++ lib/japanese_holiday.ex
@@ -51,66 +51,64 @@ defmodule JapaneseHoliday do
     is_binary(holiday_name Date.subtract date, {0, 3600, 0})
   end
 
-  defp do_holiday_name(%DateTime{year: 1973, month:  4, day: 12}), do: "hurikae"
-
-  defp do_holiday_name(%DateTime{            month: 1, day:   1}), do: "gantan"
+  defp do_holiday_name(%DateTime{            month: 1, day:   1}), do: "元日"
   defp do_holiday_name(%DateTime{year: year, month: 1, day: day} = date) when year >= 2000 do
-    if div(day-1, 7) == 1 and Date.weekday(date) == @mon, do: "seijin"
+    if div(day-1, 7) == 1 and Date.weekday(date) == @mon, do: "成人の日"
   end
-  defp do_holiday_name(%DateTime{year: year, month: 1, day: day}) when year < 2000 and day == 15, do: "seijin"
+  defp do_holiday_name(%DateTime{year: year, month: 1, day: day}) when year < 2000 and day == 15, do: "成人の日"
 
-  defp do_holiday_name(%DateTime{year: year, month: 2, day: 11}) when year >= 1967, do: "kenkoku"
-  defp do_holiday_name(%DateTime{year: 1989, month: 2, day: 24}), do: "taisou"
+  defp do_holiday_name(%DateTime{year: year, month: 2, day: 11}) when year >= 1967, do: "建国記念の日"
+  defp do_holiday_name(%DateTime{year: 1989, month: 2, day: 24}), do: "昭和天皇の大喪の礼"
 
   defp do_holiday_name(%DateTime{year: year, month: 3, day: day}) do
-    if day == vernal_equinox_day(year), do: "shunbun"
+    if day == vernal_equinox_day(year), do: "春分の日"
   end
 
-  defp do_holiday_name(%DateTime{year: year, month: 4, day: 29}) when year in 2007..9999, do: "showa"
-  defp do_holiday_name(%DateTime{year: year, month: 4, day: 29}) when year in 1989..2006, do: "midori"
-  defp do_holiday_name(%DateTime{            month: 4, day: 29}), do: "tennoh"
-  defp do_holiday_name(%DateTime{year: 1959, month: 4, day: 10}), do: "akihito"
+  defp do_holiday_name(%DateTime{year: year, month: 4, day: 29}) when year in 2007..9999, do: "昭和の日"
+  defp do_holiday_name(%DateTime{year: year, month: 4, day: 29}) when year in 1989..2006, do: "みどりの日"
+  defp do_holiday_name(%DateTime{            month: 4, day: 29}), do: "天皇誕生日"
+  defp do_holiday_name(%DateTime{year: 1959, month: 4, day: 10}), do: "皇太子明仁親王の結婚の儀"
 
-  defp do_holiday_name(%DateTime{            month: 5, day: 3}), do: "kenpoh"
-  defp do_holiday_name(%DateTime{year: year, month: 5, day: 4}) when year in 2007..9999, do: "midori"
+  defp do_holiday_name(%DateTime{            month: 5, day: 3}), do: "憲法記念日"
+  defp do_holiday_name(%DateTime{year: year, month: 5, day: 4}) when year in 2007..9999, do: "みどりの日"
   defp do_holiday_name(%DateTime{year: year, month: 5, day: 4} = date) when year in 1986..2006 do
-    if Date.weekday(date) in @tue..@sat, do: "kokumin"
+    if Date.weekday(date) in @tue..@sat, do: "国民の休日"
   end
-  defp do_holiday_name(%DateTime{            month: 5, day: 5}), do: "kodomo"
+  defp do_holiday_name(%DateTime{            month: 5, day: 5}), do: "こどもの日"
   defp do_holiday_name(%DateTime{year: year, month: 5, day: 6} = date) when year >= 2007 do
-    if Date.weekday(date) in @tue..@wed, do: "hurikae"
+    if Date.weekday(date) in @tue..@wed, do: "振替休日"
   end
 
-  defp do_holiday_name(%DateTime{year: 1993, month: 6, day: 9}), do: "norihito"
+  defp do_holiday_name(%DateTime{year: 1993, month: 6, day: 9}), do: "皇太子徳仁親王の結婚の儀"
 
   defp do_holiday_name(%DateTime{year: year, month: 7, day: day} = date) when year >= 2003 do
-    if Date.weekday(date) == @mon and div(day-1, 7) == 2, do: "umi"
+    if Date.weekday(date) == @mon and div(day-1, 7) == 2, do: "海の日"
   end
-  defp do_holiday_name(%DateTime{year: year, month: 7, day: 20}) when year >= 1996, do: "umi"
+  defp do_holiday_name(%DateTime{year: year, month: 7, day: 20}) when year >= 1996, do: "海の日"
 
-  defp do_holiday_name(%DateTime{year: year, month: 8, day: 11}) when year >= 2016, do: "yama"
+  defp do_holiday_name(%DateTime{year: year, month: 8, day: 11}) when year >= 2016, do: "山の日"
 
   defp do_holiday_name(%DateTime{year: year, month: 9, day: day} = date) do
     equinox = autumnal_equinox_day(year)
     cond do
-      day  == equinox -> "shuubun"
-      year >= 2003 and Date.weekday(date) == @mon and div(day-1, 7) == 2 -> "keirou"
-      year >= 2003 and Date.weekday(date) == @tue and day == equinox-1 -> "kokumin"
-      year >= 1966 and day == 15 -> "keirou"
+      day  == equinox -> "秋分の日"
+      year >= 2003 and Date.weekday(date) == @mon and div(day-1, 7) == 2 -> "敬老の日"
+      year >= 2003 and Date.weekday(date) == @tue and day == equinox-1 -> "国民の休日"
+      year >= 1966 and day == 15 -> "敬老の日"
       true -> nil
     end
   end
 
   defp do_holiday_name(%DateTime{year: year, month: 10, day: day} = date) when year >= 2000 do
-    if Date.weekday(date) == @mon and div(day-1, 7) == 1, do: "taiiku"
+    if Date.weekday(date) == @mon and div(day-1, 7) == 1, do: "体育の日"
   end
-  defp do_holiday_name(%DateTime{year: year, month: 10, day: 10}) when year >= 1966, do: "taiiku"
+  defp do_holiday_name(%DateTime{year: year, month: 10, day: 10}) when year >= 1966, do: "体育の日"
 
-  defp do_holiday_name(%DateTime{month: 11, day:  3}), do: "bunka"
-  defp do_holiday_name(%DateTime{month: 11, day: 23}), do: "kinrou"
-  defp do_holiday_name(%DateTime{year:  1990, month: 11, day: 12}), do: "sokui_no_rei"
+  defp do_holiday_name(%DateTime{            month: 11, day:  3}), do: "文化の日"
+  defp do_holiday_name(%DateTime{            month: 11, day: 23}), do: "勤労感謝の日"
+  defp do_holiday_name(%DateTime{year: 1990, month: 11, day: 12}), do: "即位礼正殿の儀"
 
-  defp do_holiday_name(%DateTime{year: year, month: 12, day: 23}) when year >= 1989, do: "tennoh"
+  defp do_holiday_name(%DateTime{year: year, month: 12, day: 23}) when year >= 1989, do: "天皇誕生日"
 
   defp do_holiday_name(%DateTime{}), do: nil
 
